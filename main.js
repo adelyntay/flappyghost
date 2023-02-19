35e08e8dd23faa14e0efaa29e726527bde9c4f23
ade-t <ad_elyn@hotmail.com>
2023-02-19 22:32:37 +0800

    gravity, pillars, flying function

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 const display = document.getElementById("display");
 const ghost = document.getElementById("ghost");
-const reStartButton = document.getElementById("restart");
-let fly = 0;
+const pillars = document.getElementById("pillars");
+const hole = document.getElementById("hole");
+const start = document.getElementById("start");
+
+
 let gameOver = false;
 
-/*function flying() {
-    flying = 1;
 
-}*/
+let ghostBottom = 500
+let ghostLeft = 500
+let gravity = 2
 
-reStartButton.addEventListener("click", () => {
-    fly = 0; 
-    gameOver = false;
-})
+
+function startGame() {
+    ghostBottom -= gravity
+    ghost.style.bottom = ghostBottom + "px"
+    ghost.style.left = ghostLeft + "px"
+}
+let timerID = setInterval(startGame, 20)
+
+function movePillars() {
+    hole.addEventListener("animationiteration", () => {
+        let random = -((Math.random()*300)+250);
+        hole.style.top = random + "px";
+    });
+}
+movePillars()
+
+
+function fly() {
+    ghostBottom += 50
+    ghost.style.bottom = ghostBottom + "px"
+}
+document.addEventListener("click", fly)
+
