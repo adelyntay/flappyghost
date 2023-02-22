9735c5080cccec27d3c9f50ecf7dc441964b7402
ade-t <ad_elyn@hotmail.com>
2023-02-22 21:52:18 +0800

    generate pillars

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-const display = document.getElementById("display");
+const display = document.querySelector(".display");
 const ghost = document.getElementById("ghost");
-const pillarOne = document.getElementById("pillarOne");
-/*const holeOne = document.getElementById("holeOne");
-const pillarTwo = document.getElementById("pillarTwo");
-const holeTwo = document.getElementById("holeTwo");*/
 const start = document.getElementById("start");
 const scoreValue = document.getElementById("score");
 const gameOver = document.getElementById("gameOver");
@@ -13,14 +9,6 @@ let score = 0;
 let ghostBottom = 500
 let ghostLeft = 300
 let gravity = 2
-
-let pillarOneLeft = 600
-/*let pillarTwoLeft = 800
-let holeOneLeft = 600
-let holeTwoLeft = 800*/
-let gapTop = 150
-let gapTopLeft = 600
-
 
 function startGame() {
     ghostBottom -= gravity
@@ -33,31 +21,31 @@ function startGame() {
 }
 let timerID = setInterval(startGame, 20)
 
-function movePillars() {
-    pillarOneLeft -=2
-    gapTopLeft -=2
-    let randomGapTop = ((Math.random()*150)+50)
-    /*pillarTwoLeft -=2
-    holeOneLeft -=2
-    holeTwoLeft -=2*/
-    pillarOne.style.left = pillarOneLeft + "px"
-    gapTop = randomGapTop
-    gap.style.left = gapTopLeft + "px"
-    /*pillarTwo.style.left = pillarTwoLeft + "px"
-    holeOne.style.left = holeOneLeft + "px"
-    holeTwo.style.left = holeTwoLeft + "px"*/
+function generatePillar(){
+    let pillarLeft = 500
+    let randomHeight = Math.random() * -60
+    let pillarBottom = randomHeight
+    const pillar = document.createElement("div")
+    pillar.classList.add("pillar")
+    display.appendChild(pillar)
+    pillar.style.left = pillarLeft + "px"
+    pillar.style.bottom = pillarBottom + "px"
 
-    if(pillarOneLeft === 250){ //&& holeOneLeft === 250 || pillarTwoLeft === 250 && holeTwoLeft === 250){
-        getScore();
-    }
-    if(pillarOneLeft === -50){
-        clearInterval(pillarID);
-    }
-    
+    function movePillar() {
+        pillarLeft -=2
+        pillar.style.left = pillarLeft + "px"
 
+        if (pillarLeft === -50) {
+            display.removeChild(pillar)
+        }
+        if (pillarLeft === 250) {
+            getScore()
+        }
+    }
+    setInterval(movePillar, 20)
+    setTimeout(generatePillar, 2000)
 }
-let pillarID = setInterval(movePillars, 20)
-setTimeout(movePillars, 3000)
+generatePillar()
 
 function getScore() {
     score++;
