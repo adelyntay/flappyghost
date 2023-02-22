62de6924acd657ea16258fe856b04385acb8a904
ade-t <ad_elyn@hotmail.com>
2023-02-22 14:01:43 +0800

    remove createElement

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const display = document.getElementById("display");
 const ghost = document.getElementById("ghost");
 const pillarOne = document.getElementById("pillarOne");
-const holeOne = document.getElementById("holeOne");
+/*const holeOne = document.getElementById("holeOne");
 const pillarTwo = document.getElementById("pillarTwo");
-const holeTwo = document.getElementById("holeTwo");
+const holeTwo = document.getElementById("holeTwo");*/
 const start = document.getElementById("start");
 const scoreValue = document.getElementById("score");
 const gameOver = document.getElementById("gameOver");
@@ -15,9 +15,11 @@ let ghostLeft = 300
 let gravity = 2
 
 let pillarOneLeft = 600
-let pillarTwoLeft = 800
+/*let pillarTwoLeft = 800
 let holeOneLeft = 600
-let holeTwoLeft = 800
+let holeTwoLeft = 800*/
+let gapTop = 150
+let gapTopLeft = 600
 
 
 function startGame() {
@@ -33,20 +35,29 @@ let timerID = setInterval(startGame, 20)
 
 function movePillars() {
     pillarOneLeft -=2
-    pillarTwoLeft -=2
+    gapTopLeft -=2
+    let randomGapTop = ((Math.random()*150)+50)
+    /*pillarTwoLeft -=2
     holeOneLeft -=2
-    holeTwoLeft -=2
+    holeTwoLeft -=2*/
     pillarOne.style.left = pillarOneLeft + "px"
-    pillarTwo.style.left = pillarTwoLeft + "px"
+    gapTop = randomGapTop
+    gap.style.left = gapTopLeft + "px"
+    /*pillarTwo.style.left = pillarTwoLeft + "px"
     holeOne.style.left = holeOneLeft + "px"
-    holeTwo.style.left = holeTwoLeft + "px"
+    holeTwo.style.left = holeTwoLeft + "px"*/
 
-    if(pillarOneLeft === 250 && holeOneLeft === 250 || pillarTwoLeft === 250 && holeTwoLeft === 250){
+    if(pillarOneLeft === 250){ //&& holeOneLeft === 250 || pillarTwoLeft === 250 && holeTwoLeft === 250){
         getScore();
     }
+    if(pillarOneLeft === -50){
+        clearInterval(pillarID);
+    }
+    
 
 }
 let pillarID = setInterval(movePillars, 20)
+setTimeout(movePillars, 3000)
 
 function getScore() {
     score++;
@@ -66,12 +77,3 @@ function gameEnd() {
     document.removeEventListener("click", fly)
     gameOver.innerHTML = ("Game Over");
 }
-
-/*pillars createElement
-
-let pillarsLeft = 800
-const pillars = document.createElement("div");
-display.appendChild(pillars);
-pillars.innerHTML = ("blue");
-pillars.style.left = pillarsLeft + "px";
-pillars.classList.add("pillars");*/
