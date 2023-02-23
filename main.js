9d562b6caddb376fbb4e9908549149d582e405fd
ade-t <ad_elyn@hotmail.com>
2023-02-23 21:56:44 +0800

    collision detection

--- main.js
+++ main.js
@@ -1,60 +1,65 @@
-const display = document.querySelector(".display");
-const ghost = document.getElementById("ghost");
-const start = document.getElementById("start");
-const scoreValue = document.getElementById("score");
-const gameOver = document.getElementById("gameOver");
+const display = document.querySelector(".display")
+const ghost = document.querySelector(".ghost")
+const scoreValue = document.getElementById("score")
+const gameOver = document.getElementById("gameOver")
 
-let score = 0;
-let stopGame = false 
-
-let ghostBottom = 500
-let ghostLeft = 300
+let ghostBottom = 200
+let ghostLeft = 220
 let gravity = 2
-
+let gap = 430
+let score = 0
+let stopGame = false 
 
 function startGame() {
     ghostBottom -= gravity
     ghost.style.bottom = ghostBottom + "px"
     ghost.style.left = ghostLeft + "px"
-    
-    if(ghostBottom === 0 || ghostBottom === 520){
-        gameEnd();
-    }   
 }
 let timerID = setInterval(startGame, 20)
 
+function fly() {
+    if (ghostBottom < 500) ghostBottom += 50
+    ghost.style.bottom = ghostBottom + "px"
+}
+document.addEventListener("click", fly)
+
 function generatePillar(){
-    let pillarLeft = 800
-
-    let randomHeight= Math.floor(Math.random() * 200) + 150;
+    let pillarLeft = 500
+    let randomHeight = Math.random() * 60
+    let pillarBottom = randomHeight
     const pillar = document.createElement("div")
-    pillar.classList.add("pillar")
+    const topPillar = document.createElement("div")
+    if (!stopGame) {
+        pillar.classList.add("pillar")
+        topPillar.classList.add("topPillar")
+    }
     display.appendChild(pillar)
+    display.appendChild(topPillar)
     pillar.style.left = pillarLeft + "px"
-    pillar.style.height = randomHeight + "px"
+    topPillar.style.left = pillarLeft + "px"
+    pillar.style.bottom = pillarBottom + "px"
+    topPillar.style.bottom = pillarBottom + gap + "px"
     
-    let randomHeightTwo = Math.floor(Math.random() * 200) + 100;
-    const pillarTwo = document.createElement("div")
-    pillarTwo.classList.add("pillarTwo")
-    display.appendChild(pillarTwo)
-    pillarTwo.style.left = pillarLeft + "px"
-    pillarTwo.style.height = randomHeightTwo + "px"
-
     function movePillar() {
         pillarLeft -=2
         pillar.style.left = pillarLeft + "px"
-        pillarTwo.style.left = pillarLeft + "px"
+        topPillar.style.left = pillarLeft + "px"
 
         if (pillarLeft === -50) {
             display.removeChild(pillar)
+            display.removeChild(topPillar)
         }
-        if (pillarLeft === 250) {
-            getScore()
-        }
-        
+        if (
+            pillarLeft > 200 && pillarLeft < 280 && ghostLeft === 220 &&
+            (ghostBottom < pillarBottom + 153 || ghostBottom > ghostBottom + gap -200) ||
+            ghostBottom === 0
+            ) {
+            gameEnd()
+            clearInterval(pillarID)
+            }
     }
-    setInterval(movePillar, 20)
-    setTimeout(generatePillar, 2000)
+    let pillarID = setInterval(movePillar, 20)
+    if (!stopGame) setTimeout(generatePillar, 3000)
 }
 generatePillar()
 
@@ -64,14 +69,6 @@ function getScore() {
     scoreValue.innerHTML = ("Score :" + score);
     }
 }
-
-function fly() {
-    ghostBottom += 50
-    ghost.style.bottom = ghostBottom + "px"
-    console.log(fly)
-}
-document.addEventListener("click", fly)
-
 
 function gameEnd() {
     clearInterval(timerID)
